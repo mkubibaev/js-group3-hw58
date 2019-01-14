3d950b0cdc8fa13d3bfcc45eb3e9ef24acf07104
mkubibaev <mkubibaev@gmail.com>
2019-01-14 18:28:18 +0600

    fixed Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,40 +2,44 @@ import React from 'react';
 import Button from "../Button/Button";
 
 const Modal = props => {
-    return (
-        <div className="modal"
-             style={{
-                 display: props.show ? "block": "none"
-             }}>
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">{props.title}</h5>
-                        <button
-                            type="button"
-                            className="close"
-                            onClick={props.closed}
-                        >
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        {props.children}
-                    </div>
-                    <div className="modal-footer">
-                        {props.buttons.map(button => (
-                            <Button
-                                key={button.label}
-                                type={button.type}
-                                label={button.label}
-                                clicked={button.clicked}
-                            />
-                        ))}
+    let modal = null;
+
+    if (props.show) {
+        modal = (
+            <div className="modal">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{props.title}</h5>
+                            <button
+                                type="button"
+                                className="close"
+                                onClick={props.closed}
+                            >
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {props.children}
+                        </div>
+                        <div className="modal-footer">
+                            {props.buttons.map(button => (
+                                <Button
+                                    key={button.label}
+                                    type={button.type}
+                                    label={button.label}
+                                    clicked={button.clicked}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
+
+
+    return modal;
 };
 
 export default Modal;
